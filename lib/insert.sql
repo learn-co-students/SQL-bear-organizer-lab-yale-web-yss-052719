a691ce5cbd7f58b0bd9740eec1568d9fa1af16b7
rinakawamura <rina.kawamura@yale.edu>
2019-06-04 11:30:44 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "F", "brown", "gentle", TRUE);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1, "M", "brown", "excited", TRUE);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Tabitha", 3, "F", "black", "gentle", TRUE);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 6, "M", "brown", "gentle", TRUE);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Melissa", 3, "F", "brown", "gentle", TRUE);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Grinch", 3, "F", "brown", "mean", TRUE);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Wendy", 3, "F", "brown", "gentle", TRUE);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (NULL, 3, "F", "brown", "gentle", TRUE);
