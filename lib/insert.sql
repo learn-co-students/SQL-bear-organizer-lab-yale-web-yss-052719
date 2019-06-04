dde0071579672a533a1b7d7ddbfcb58bd88231e4
Elijah <efloren@siue.edu>
2019-06-04 17:36:23 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Smokey", 54, "Male", "Brown", "Chill", "ALIVE");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Po", 24, "Male", "Black/White", "Dragon-Warrior", "ALIVE");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Aemon-Khan-of_Bears", 532, "Male", "Black", "Wise", "DEAD");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rhen", 17, "Female", "Pink", "Horny", "ALIVE");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 10, "Male", "Grey", "Pissed", "ALIVE");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("UltraMechaBear", 03, "None", "Steel", "DESTROY", "ALIVE");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("DinoBear", 97, "All", "Green", "Angry", "DEAD");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("WildBear", 20, "Male", "Brown", "Hungry", "ALIVE");
